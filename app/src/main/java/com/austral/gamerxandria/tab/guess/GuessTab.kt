b7bb26ad22d6daf07688d38ed085e6a06c2a8718
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-03-26 19:59:51 -0300

    Added a stand by image for the guess tab

--- app/src/main/java/com/austral/gamerxandria/tab/guess/GuessTab.kt
+++ app/src/main/java/com/austral/gamerxandria/tab/guess/GuessTab.kt
@@ -1,11 +1,23 @@
 package com.austral.gamerxandria.tab.guess
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import com.austral.gamerxandria.R
 
 @Composable
 fun GuessTab() {
-     Text(
-         text = "Guess"
-     )
+    Column {
+        Box(modifier = Modifier.clip(MaterialTheme.shapes.medium)) {
+            Image(
+                painterResource(R.drawable.stand_by_image),
+                contentDescription = "Stand ready for my arrival, worm",
+            )
+        }
+    }
 }
