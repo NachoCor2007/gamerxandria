bcdd9ae8fe645c9cb55db9c52b87c89b80ef2728
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-03-26 11:27:36 -0300

    Changed how loop of mocked games operated, made columns and rows Lazy, so they can be scrollable (apart from their lazy approach)

--- app/src/main/java/com/austral/gamerxandria/tab/library/LibraryTab.kt
+++ app/src/main/java/com/austral/gamerxandria/tab/library/LibraryTab.kt
@@ -1,46 +1,48 @@
 package com.austral.gamerxandria.tab.library
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun LibraryTab() {
-    Column(
-        modifier = Modifier.fillMaxSize().background(color = Color.Red)
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "My library",
-            fontSize = 32.sp
-        )
-        Shelf()
+        item {
+            repeat(10) {
+                Shelf()
+            }
+        }
     }
 }
 
 @Composable
 fun Shelf() {
     Column(
-        modifier = Modifier.fillMaxWidth().padding(0.dp, 8.dp).background(Color.Green)
+        modifier = Modifier.fillMaxWidth().padding(0.dp, 8.dp)
     ) {
         Text(
-            text = "Colection A"
+            text = "A collection",
+            fontSize = 32.sp
         )
-        Row {
-            for (i in 1..10) {
-                GameCard()
+        LazyRow {
+            item {
+                repeat(10) {
+                    GameCard()
+                }
             }
         }
     }
