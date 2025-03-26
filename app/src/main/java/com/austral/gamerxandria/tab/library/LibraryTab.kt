9a887e3609b7c4ad736a58043b5438ad77410da1
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-03-26 12:46:06 -0300

    Unused deps

--- app/src/main/java/com/austral/gamerxandria/tab/library/LibraryTab.kt
+++ app/src/main/java/com/austral/gamerxandria/tab/library/LibraryTab.kt
@@ -1,20 +1,9 @@
 package com.austral.gamerxandria.tab.library
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.austral.gamerxandria.components.Shelf
 
 @Composable
