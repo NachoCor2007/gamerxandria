c990eea455fb0b08b7ef8adff82601e17d1aed4e
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-04-09 19:46:15 -0300

    GameView stand by

---
+++ app/src/main/java/com/austral/gamerxandria/components/GameView.kt
@@ -0,0 +1,11 @@
+package com.austral.gamerxandria.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GameView() {
+    Text(
+        text = "Jueguito"
+    )
+}
