9c265328c238b73c98aebcce61a2fbbbccb5dfdf
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-04-16 20:20:26 -0300

    GameView properties display

--- app/src/main/java/com/austral/gamerxandria/components/GameView.kt
+++ app/src/main/java/com/austral/gamerxandria/components/GameView.kt
@@ -1,11 +1,76 @@
 package com.austral.gamerxandria.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.austral.gamerxandria.R
 
 @Composable
 fun GameView() {
-    Text(
-        text = "Jueguito"
-    )
+    Column(
+        modifier = Modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 8.dp)
+                .background(Color.Transparent),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Jueguito",
+                textAlign = TextAlign.Center,
+                fontSize = 24.sp
+            )
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize().verticalScroll(state = rememberScrollState(),enabled = true)
+        ) {
+            Image(
+                painterResource(R.drawable.stand_by_image),
+                contentDescription = "Stand ready for my arrival, worm",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .fillMaxWidth()
+                    .height(256.dp)
+            )
+            Text(
+                text = "Game status",
+                fontSize = 16.sp,
+                modifier = Modifier.padding(16.dp)
+            )
+            Text(
+                text = "Game description",
+                fontSize = 24.sp,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
 }
