25e9a6b572c631c83b21955f789756860d99d2de
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-04-19 01:56:28 -0300

    Added new mock for search tab.

--- app/src/main/java/com/austral/gamerxandria/mock/ShelfMock.kt
+++ app/src/main/java/com/austral/gamerxandria/mock/ShelfMock.kt
@@ -8,3 +8,9 @@ var userShelvesMock = listOf<Shelf>(
     Shelf("In progress", listOf(1, 3, 4, 8, 9, 10, 12)),
     Shelf("Backlog", listOf(2))
 )
+
+var searchShelvesMock = listOf<Shelf>(
+    Shelf("Based on your library", listOf(1, 2, 3)),
+    Shelf("Trending", listOf(2, 3)),
+    Shelf("All-star", listOf(1, 3, 4, 8, 9, 10, 12))
+)
