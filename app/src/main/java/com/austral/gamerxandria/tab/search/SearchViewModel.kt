2d078d7ad74490ab442b5e3d7ee49a74cce13258
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-04-24 02:34:50 -0300

    Problem: for some reason, sometimes there is no cover.url when receiving the respons.

--- app/src/main/java/com/austral/gamerxandria/tab/search/SearchViewModel.kt
+++ app/src/main/java/com/austral/gamerxandria/tab/search/SearchViewModel.kt
@@ -1,19 +1,51 @@
 package com.austral.gamerxandria.tab.search
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.austral.gamerxandria.apiManager.ApiServiceImpl
 import com.austral.gamerxandria.mock.searchShelvesMock
 import com.austral.gamerxandria.model.Shelf
+import com.austral.gamerxandria.model.VideoGame
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor() : ViewModel() {
-    private var _shelves = MutableStateFlow(searchShelvesMock)
-    val shelves = _shelves.asStateFlow()
+class SearchViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val apiServiceImpl: ApiServiceImpl
+) : ViewModel() {
+    private var _videoGames = MutableStateFlow(listOf<VideoGame>())
+    val videoGames = _videoGames.asStateFlow()
 
-    fun retrieveShelves(): List<Shelf> {
-        return shelves.value
+    private var _loading = MutableStateFlow(false)
+    val loading = _loading.asStateFlow()
+
+    private var _showRetry = MutableStateFlow(false)
+    val showRetry = _showRetry.asStateFlow()
+
+    init {
+        searchVideoGamesNames("")
+    }
+
+    fun searchVideoGamesNames(query: String) {
+        _loading.value = true
+
+        apiServiceImpl.searchVideoGamesByName(
+            name = query,
+            context = context,
+            onSuccess = {
+                _videoGames.value = it
+                _showRetry.value = false
+            },
+            onFail = {
+                _showRetry.value = true
+            },
+            loadingFinished = {
+                _loading.value = false
+            }
+        )
     }
 }
