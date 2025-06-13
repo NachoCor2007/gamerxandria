1d624446f04994a2118d4660ccb48a7385903ce6
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-06-13 12:25:43 -0300

    login

---
+++ app/src/main/java/com/austral/gamerxandria/tab/profile/ProfileTab.kt
@@ -0,0 +1,18 @@
+package com.austral.gamerxandria.tab.profile
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileTab() {
+    Column {
+
+
+        ProfileContent()
+    }
+}
+
+@Composable
+fun ProfileContent() {
+    TODO("Not yet implemented")
+}
